b95562fc6d2ab6c6e799869c0ebb5a8feeb384a1
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2025-04-17 08:11:45 +0100

    Extract methods for readability
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- main.go
+++ main.go
@@ -13,35 +13,39 @@ import (
 )
 
 func main() {
-
 	var write bool
+	var verbose bool
 
-	if len(os.Args) <= 1 {
+	flag.BoolVar(&write, "write", false, "Write changes to the file")
+	flag.BoolVar(&verbose, "verbose", false, "Enable verbose output")
+
+	flag.Parse()
+
+	if len(flag.Args()) < 1 {
 		fmt.Fprintf(os.Stderr, "gha-bump ./workflow.yaml\n")
 		os.Exit(1)
 	}
 
-	flag.BoolVar(&write, "write", false, "Write changes to the file")
-
-	flag.Parse()
-
-	target := os.Args[1]
-	if _, err := os.Stat(target); err != nil {
+	target := flag.Args()[0]
+	if err := run(write, verbose, target); err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
 		os.Exit(1)
+	}
+}
+
+func run(write, verbose bool, target string) error {
+	if _, err := os.Stat(target); err != nil {
+		return err
 	}
 
 	data, err := os.ReadFile(target)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
-		os.Exit(1)
+		return err
 	}
 
-	// Parse the workflow file
-	workflow, err := parseWorkflow(data)
+	workflow, err := loadWorkflow(data)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
-		os.Exit(1)
+		return err
 	}
 
 	clientWithoutRedirects := &http.Client{
@@ -50,30 +54,54 @@ func main() {
 		},
 	}
 
-	fmt.Println("Workflow parsed successfully.")
+	replacements, err := processJobs(workflow, clientWithoutRedirects, verbose)
+	if err != nil {
+		return err
+	}
 
+	if verbose && len(replacements) > 0 {
+		fmt.Println("Detected following replacements:- ")
+		for old, new := range replacements {
+			fmt.Printf("  %s -> %s\n", old, new)
+		}
+	}
+
+	if write {
+		if err := applyReplacements(data, replacements, target); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func loadWorkflow(data []byte) (map[string]interface{}, error) {
+	return parseWorkflow(data)
+}
+
+func processJobs(workflow map[string]interface{}, client *http.Client, verbose bool) (map[string]string, error) {
 	jobs, ok := workflow["jobs"].(map[string]interface{})
 	if !ok {
-		fmt.Fprintf(os.Stderr, "Error: jobs not found in workflow\n")
-		os.Exit(1)
+		return nil, fmt.Errorf("jobs not found in workflow")
 	}
+
+	replace := map[string]string{}
 
 	for jobName, job := range jobs {
 		jobMap, ok := job.(map[string]interface{})
 		if !ok {
-			fmt.Fprintf(os.Stderr, "Error: job %s is not a map\n", jobName)
-			os.Exit(1)
+			return nil, fmt.Errorf("job %s is not a map", jobName)
 		}
 
-		steps := jobMap["steps"].([]interface{})
-
-		replace := map[string]string{}
+		steps, ok := jobMap["steps"].([]interface{})
+		if !ok {
+			return nil, fmt.Errorf("steps not found or not a list in job %s", jobName)
+		}
 
 		for _, step := range steps {
 			stepMap, ok := step.(map[string]interface{})
 			if !ok {
-				fmt.Fprintf(os.Stderr, "Error: step is not a map in job %s\n", jobName)
-				os.Exit(1)
+				return nil, fmt.Errorf("step is not a map in job %s", jobName)
 			}
 
 			var name string
@@ -98,73 +126,66 @@ func main() {
 				st = uses
 			}
 
-			if len(uses) > 0 {
-				fmt.Printf("  %s: %s\n", st, uses)
-			} else {
-				fmt.Printf("  %s\n", st)
+			if verbose {
+				if len(uses) > 0 {
+					fmt.Printf("  %s: %s\n", st, uses)
+				} else {
+					fmt.Printf("  %s\n", st)
+				}
 			}
 
 			if len(uses) > 0 {
-
 				ownerRepo, currentVer, ok := strings.Cut(uses, "@")
 				if ok {
 					if currentVer != "master" {
-
 						owner, repo, ok := strings.Cut(ownerRepo, "/")
 						if ok {
-							version, err := getLatestVersion(clientWithoutRedirects, owner, repo)
+							version, err := getLatestVersion(client, owner, repo)
 							if err != nil {
-								fmt.Fprintf(os.Stderr, "Error: %s\n", err)
-								os.Exit(1)
+								return nil, err
 							}
 
 							oldSemver, err := semver.NewVersion(currentVer)
 							if err != nil {
-								fmt.Fprintf(os.Stderr, "Error: %s\n", err)
-								os.Exit(1)
+								return nil, err
 							}
 
 							newSemver, err := semver.NewVersion(version)
 							if err != nil {
-								fmt.Fprintf(os.Stderr, "Error: %s\n", err)
-								os.Exit(1)
+								return nil, err
 							}
 
 							if newSemver.Major() > oldSemver.Major() {
 								shortVer := fmt.Sprintf("v%d", newSemver.Major())
-
 								replace[uses] = shortVer
 							}
-
 						}
 					}
 				}
-
 			}
-
 		}
-
-		fmt.Println("Detected following replacements:- ")
-
-		dataSt := string(data)
-
-		for old, new := range replace {
-			fmt.Printf("  %s -> %s\n", old, new)
-
-			oldFull := old
-			workflowPath, _, _ := strings.Cut(oldFull, "@")
-			newFull := fmt.Sprintf("%s@%s", workflowPath, new)
-
-			dataSt = strings.ReplaceAll(dataSt, oldFull, newFull)
-		}
-
-		if err := os.WriteFile(target, []byte(dataSt), 0644); err != nil {
-			fmt.Fprintf(os.Stderr, "Error: %s\n", err)
-			os.Exit(1)
-		}
-
 	}
 
+	return replace, nil
+}
+
+func applyReplacements(data []byte, replacements map[string]string, target string) error {
+	if len(replacements) == 0 {
+		return nil
+	}
+	dataSt := string(data)
+	for old, new := range replacements {
+		oldFull := old
+		workflowPath, _, _ := strings.Cut(oldFull, "@")
+		newFull := fmt.Sprintf("%s@%s", workflowPath, new)
+		dataSt = strings.ReplaceAll(dataSt, oldFull, newFull)
+	}
+
+	if err := os.WriteFile(target, []byte(dataSt), 0644); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func parseWorkflow(data []byte) (map[string]interface{}, error) {
